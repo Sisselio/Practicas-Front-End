d4783bd68f92bee1e464ac93d4cdf10489b26f2b
Ruben Collazo <rubencollazonaranjo@gmail.com>
2025-10-08 17:45:16 +0200

    chore(exercises): pasted exercise definition

---
+++ tsExercises/exercise-1.ts
@@ -0,0 +1,3 @@
+// Crea un objeto bird que tenga los siguientes datos: name, canEat, canDrink, canSleep, canFly
+// Crea un objero dog que tenga los mismos datos que pájaro, pero que a demás tenga: race, age
+// Crea los tipos/interfaces necesarios para poder trabajar con estos datos en TypeScript.
