71dd3d61ca6a572c78a71de0e87e95920ac39d3b
Ruben Collazo <rubencollazonaranjo@gmail.com>
2025-10-09 09:12:27 +0200

    chore(exercises): finished Ts exercise 1

--- tsExercises/exercise-1.ts
+++ tsExercises/exercise-1.ts
@@ -1,3 +1,33 @@
 // Crea un objeto bird que tenga los siguientes datos: name, canEat, canDrink, canSleep, canFly
 // Crea un objero dog que tenga los mismos datos que pájaro, pero que a demás tenga: race, age
 // Crea los tipos/interfaces necesarios para poder trabajar con estos datos en TypeScript.
+
+interface Bird {
+  name: string;
+  canEat: boolean;
+  canDrink: boolean;
+  canSleep: boolean;
+  canFly: boolean;
+}
+
+interface Dog extends Bird {
+  race: string;
+  age: number;
+}
+const bird: Bird = {
+  name: "Pajarete",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: true,
+};
+
+const dog: Dog = {
+  name: "Perrillo",
+  race: "Labrador",
+  age: 5,
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: false,
+};
